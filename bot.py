2740ce187f042a02a25e98cb33e39b6c777036e9
Abhijit Tripathy <52751963+Abhijit2505@users.noreply.github.com>
2020-07-13 11:31:43 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,26 @@
+import tweepy
+import time
+
+CONSUMER_KEY = '2bafyvAn0YKWMjs0Wbtd3wBBr'
+COMSUMER_SECRET = 'sm3oWoYJgVe8Q9y5fGSaKgvvpnK5KuJ3OHsTpvjgRg0YKgHUnT'
+ACCESS_KEY = '811384273911615488-8FzvFZVvzgHgZ76NYsSRGnuwXPZxQOh'
+ACCESS_SECRET = '60ObqzXFatJuuglx2XoP8WA3AW4Y2wmsoguTFiHxt4KY8'
+auth = tweepy.OAuthHandler(CONSUMER_KEY,COMSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY,ACCESS_SECRET)
+
+api = tweepy.API(auth,wait_on_rate_limit=True, wait_on_rate_limit_notify=true)
+user = api.me()
+search = '#nature'
+numTweet = 500
+
+for tweet in tweepy.Cursor(api.search,search).items(numTweet):
+    try:
+        print('Tweet Liked')
+        tweet.favourite()
+        print('Retweet done')
+        tweet.retweet()
+        time.sleep(10)
+    except tweepy.Tweeperror as e:
+        print(e.reason)
+    except Stopiteration:
+        break
