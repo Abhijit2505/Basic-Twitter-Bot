b3abf2aafe10e09eaec7ca95a0eb2215f2c271f8
Abhijit Tripathy <52751963+Abhijit2505@users.noreply.github.com>
2020-07-15 21:05:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,16 @@
 import tweepy
 import time
 
-CONSUMER_KEY = '2bafyvAn0YKWMjs0Wbtd3wBBr'
-COMSUMER_SECRET = 'sm3oWoYJgVe8Q9y5fGSaKgvvpnK5KuJ3OHsTpvjgRg0YKgHUnT'
-ACCESS_KEY = '811384273911615488-8FzvFZVvzgHgZ76NYsSRGnuwXPZxQOh'
-ACCESS_SECRET = '60ObqzXFatJuuglx2XoP8WA3AW4Y2wmsoguTFiHxt4KY8'
+CONSUMER_KEY = 'secret-key'
+COMSUMER_SECRET = 'secret-key'
+ACCESS_KEY = 'secret-key'
+ACCESS_SECRET = 'secret-key'
 auth = tweepy.OAuthHandler(CONSUMER_KEY,COMSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY,ACCESS_SECRET)
 
 api = tweepy.API(auth,wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 user = api.me()
-search = '#nature'
+search = '#technology'
 numTweet = 500
 
 for tweet in tweepy.Cursor(api.search,search).items(numTweet):
